f06d71d2790ea4cf80bb2c2d2c80c6541e9fffd7
Esteban <estebanmondaca@gmail.com>
2015-04-06 23:08:06 -0400

    commit desde netbeans

--- RegPersonal/src/java/servlets/AdminDBServlet.java
+++ RegPersonal/src/java/servlets/AdminDBServlet.java
@@ -69,25 +69,13 @@ public class AdminDBServlet extends HttpServlet {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+
     @Override
     public String getServletInfo() {
         return "Short description";
@@ -206,13 +194,11 @@ public class AdminDBServlet extends HttpServlet {
             
             for (int x = 0; x < registros.size(); x++) {
                 // Recuperar y Mostrar los datos ...
-                PersonalBean personal = (PersonalBean) registros.get(x);
+                PersonalBean personal = (PersonalBean)registros.get(x);
                 
-                out.println(personal.getCodigo() + " - " + personal.getNombre() + 
-                            " - " + personal.getDepartamento() + "<HR>");
+                out.println(personal.getCodigo() + " - " + personal.getNombre() + " - " + personal.getDepartamento() + "<HR>");
                 
             }
-            
             
             out.println("<BR><BR><BR><HR>");
             
